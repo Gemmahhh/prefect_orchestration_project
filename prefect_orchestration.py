3e7c9f6e9bd25a238994fedf14fe36655fb448d6
Okpe Chiamaka Doris <okpeamaka8@gmail.com>
2025-02-01 20:28:58 +0100

    New commit

--- prefect_orchestration.py
+++ prefect_orchestration.py
@@ -1,6 +1,6 @@
 from prefect import flow, task
 from prefect_aws.s3 import S3Bucket
-from prefect_dbt.cloud import DbtCloudCredentials, run_dbt_cloud_job
+from prefect_dbt.cloud import DbtCloudCredentials, DbtCloudJob
 from prefect_snowflake.database import SnowflakeConnector
 import boto3
 
@@ -16,7 +16,7 @@ def upload_to_s3(local_file: str, s3_bucket_block_name: str, s3_file: str):
 
 # Task 2: Load data from S3 to Snowflake
 @task
-def load_to_snowflake(s3_path: str, snowflake_block_name: str, table_name: str):
+def load_to_snowflake(stage_path: str, snowflake_block_name: str, table_name: str):
     """
     Loads data from S3 into Snowflake using a Prefect SnowflakeConnector block.
     """
@@ -25,7 +25,7 @@ def load_to_snowflake(s3_path: str, snowflake_block_name: str, table_name: str):
     # SQL to copy data from S3 to Snowflake
     query = f"""
     COPY INTO {table_name}
-    FROM '{s3_path}'
+    FROM '{stage_path}'
     FILE_FORMAT = (TYPE = 'CSV' SKIP_HEADER = 1);
     """
 
@@ -35,14 +35,31 @@ def load_to_snowflake(s3_path: str, snowflake_block_name: str, table_name: str):
             print(f"Data loaded successfully into Snowflake table: {table_name}")
 
 # Task 3: Trigger a dbt Cloud job
+# @task
+# def trigger_dbt_cloud_job(dbt_block_name: str, job_id: int):
+#     """
+#     Triggers a dbt Cloud job using a Prefect DbtCloudCredentials block.
+#     """
+#     dbt_credentials = DbtCloudCredentials.load(dbt_block_name)
+#     dbt_job = DbtCloudJob(credentials=dbt_credentials, job_id=job_id)
+#     dbt_job.run()
+#     print(f"Triggered dbt Cloud job with Job ID: {job_id}")
+
+
 @task
 def trigger_dbt_cloud_job(dbt_block_name: str, job_id: int):
     """
     Triggers a dbt Cloud job using a Prefect DbtCloudCredentials block.
     """
     dbt_credentials = DbtCloudCredentials.load(dbt_block_name)
-    job_run = run_dbt_cloud_job(dbt_cloud_credentials=dbt_credentials, job_id=job_id)
-    print(f"Triggered dbt Cloud job. Run ID: {job_run}")
+    print(f"Loaded dbt credentials: {dbt_credentials}")
+    #dbt_job = DbtCloudJob(credentials=dbt_credentials, job_id=job_id)
+    dbt_job = DbtCloudJob(dbt_cloud_credentials=dbt_credentials, job_id=job_id)
+    dbt_job.trigger()  
+    print(f"Triggered dbt Cloud job with Job ID: {job_id}")
+
+
+
 
 # Prefect Flow
 @flow(name="Data Pipeline Orchestration")
@@ -56,10 +73,12 @@ def data_pipeline_flow():
     # Parameters
     local_file = "e_commerce_dataset.csv"  
     s3_bucket_block_name = "my-s3-bucket-block"  
-    s3_file = "prefect_pipeline_data/e_commerce.csv"  
+    # s3_file = "prefect_pipeline_data/e_commerce.csv"  
+    s3_file = "e_commerce.csv"
     snowflake_block_name = "my-snowflake-connector"  
     table_name = "raw_schema.e_commerce"  
-    s3_path = f"s3://my-prefect-s3-bucket/{s3_file}"  
+    # s3_path = f"s3://my-prefect-s3-bucket/{s3_file}"
+    stage_path = f"@my_s3_stage/{s3_file}"  
     dbt_block_name = "my-dbt-cloud-credentials"  
     dbt_job_id = 70471823420051  
 
@@ -68,7 +87,7 @@ def data_pipeline_flow():
     upload_to_s3(local_file, s3_bucket_block_name, s3_file)
 
     # Step 2: Load data from S3 to Snowflake
-    load_to_snowflake(s3_path, snowflake_block_name, table_name)
+    load_to_snowflake(stage_path, snowflake_block_name, table_name)
 
     # Step 3: Trigger a dbt Cloud job for transformations
     trigger_dbt_cloud_job(dbt_block_name, dbt_job_id)
